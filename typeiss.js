8a3d354d6eb686b4bef5ab430123e3e9b3ce3aec
xc <824384515@qq.com>
2018-11-05 16:47:55 +0800

    delete redundant code

--- typeiss.js
+++ typeiss.js
@@ -2,7 +2,6 @@
 let typeiss = (a,b) => {
     let c = (a) => Object.prototype.toString.call(a).replace(/\[|\]|object| /g,''),
         d = c(a)
-        e = ['Undefined','Null','Boolean','Number','String','Function','Array','Object','Date','RegExp','Set','Map']
     if(c(b) === 'Array'){
         if(b.indexOf(d) != -1){
             return true
